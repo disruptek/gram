1d1810266f98e0ef46630a4d40eede09b74cc676
Andy Davidoff <andy+github@disruptek.com>
2020-05-27 00:34:36 -0400

    start proper docs

--- gram.nim
+++ gram.nim
@@ -3,12 +3,16 @@ import std/lists
 import std/intsets
 
 type
-  Graph*[N, E] = ref GraphObj[N, E]
+  Graph*[N, E] = ref GraphObj[N, E]    ## A collection of nodes and edges.
+  ## Nodes have a user-supplied `.value` of type `N`.
+  ## Edges have a user-supplied `.value` of type `E`.
   GraphObj[N, E] = object
     nodes: Nodes[N, E]
     members: IntSet
 
-  Node*[N, E] = ref NodeObj[N, E]
+  Node*[N, E] = ref NodeObj[N, E]      ## A node in the graph.
+  ## Nodes have a user-supplied `.value` of type `N`.
+  ## Edges have a user-supplied `.value` of type `E`.
   NodeObj[N, E] = object
     value*: N
     id: int
@@ -19,7 +23,9 @@ type
     peers: IntSet
   Nodes[N, E] = DoublyLinkedList[Node[N, E]]
 
-  Edge*[N, E] = ref EdgeObj[N, E]
+  Edge*[N, E] = ref EdgeObj[N, E]      ## An edge connects two nodes.
+  ## Nodes have a user-supplied `.value` of type `N`.
+  ## Edges have a user-supplied `.value` of type `E`.
   EdgeObj[N, E] = object
     value*: E
     id: int
@@ -28,7 +34,28 @@ type
     target: Node[N, E]
   Edges[N, E] = DoublyLinkedList[Edge[N, E]]
 
+proc newNodes[N, E](): Nodes[N, E] =
+  ## Create a new container for nodes.
+  result = initDoublyLinkedList[Node[N, E]]()
+
+proc newEdges[N, E](): Edges[N, E] =
+  ## Create a new container for edges.
+  result = initDoublyLinkedList[Edge[N, E]]()
+
+proc newGraph*[N, E](): Graph[N, E] =
+  ## Create a new graph; nodes will hold `N` while edges will hold `E`.
+  runnableExamples:
+    type
+      MyNode = int
+      MyEdge = string
+    var g = newGraph[MyNode, MyEdge]()
+
+  result = Graph[N, E]()
+  result.nodes = newNodes[N, E]()
+  result.members = initIntSet()
+
 proc `=destroy`[N, E](node: var NodeObj[N, E]) =
+  ## Prepare a node for destruction.
   clear(node.edges)
   clear(node.peers)
   for item in nodes(node.incoming):
@@ -39,47 +66,45 @@ proc `=destroy`[N, E](node: var NodeObj[N, E]) =
   node.id = 0
 
 proc `=destroy`[N, E](edge: var EdgeObj[N, E]) =
+  ## Prepare an `edge` for destruction.
   edge.source = nil
   edge.target = nil
   # just, really fuck this thing up
   edge.id = 0
 
 proc clear[N, E](graph: var GraphObj[N, E]) =
+  ## Empty a `graph` of all nodes and edges.
   clear(graph.members)
   for item in nodes(graph.nodes):
     remove(graph.nodes, item)
 
 proc clear*[N, E](graph: var Graph[N, E]) =
+  ## Empty a `graph` of all nodes and edges.
+  runnableExamples:
+    var g = newGraph[int, string]()
+    g.add 3
+    clear(g)
+    assert len(g) == 0
+
   clear(graph[])
 
 proc `=destroy`[N, E](graph: var GraphObj[N, E]) =
+  ## Prepare a `graph` for destruction.
   clear(graph)
 
-proc newNodes*[N, E](): Nodes[N, E] =
-  ## create a new container holding nodes
-  result = initDoublyLinkedList[Node[N, E]]()
-
-proc newEdges[N, E](): Edges[N, E] =
-  ## create a new container holding edges; not public
-  result = initDoublyLinkedList[Edge[N, E]]()
-
-proc newGraph*[N, E](): Graph[N, E] =
-  result = Graph[N, E]()
-  result.nodes = newNodes[N, E]()
-  result.members = initIntSet()
-
 proc embirth[N, E](obj: var Node[N, E] | var Edge[N, E]) =
+  ## Assign a birthday (and a unique identifier) to a node or edge.
   obj.birth = getMonoTime()
   obj.id = ticks(obj.birth).int
 
 proc newEdge[N, E](source: Node[N, E]; value: E;
                    target: Node[N, E]): Edge[N, E] =
-  ## create a new edge of the given value
+  ## Create a new edge between `source` and `target` of the given `value`.
   result = Edge[N, E](source: source, value: value, target: target)
   embirth(result)
 
 proc newNode[N, E](value: N): Node[N, E] =
-  ## create a new node of the given value
+  ## Create a new node of the given `value`.
   result = Node[N, E](value: value,
                       incoming: newEdges[N, E](),
                       outgoing: newEdges[N, E]())
@@ -88,62 +113,93 @@ proc newNode[N, E](value: N): Node[N, E] =
   embirth(result)
 
 proc del*[N, E](graph: var Graph[N, E]; node: Node[N, E]) =
-  ## remove a node from the graph
+  ## Remove a `node` from the `graph`.
+  ## Has no effect if the `node` is not in the `graph`.
+  ## Not O(1) yet.
+  runnableExamples:
+    var g = newGraph[int, string]()
+    g.add 3
+    let n = g[3]
+    g.del n
+    assert len(g) == 0
+
   if node.id in graph.members:
     for item in nodes(graph.nodes):
-      if item.id == node.id:
+      if item.value.id == node.id:
         remove(graph.nodes, item)
-        excl graph.members, item.id
+        excl graph.members, item.value.id
         break
 
 proc add*[N, E](graph: var Graph[N, E]; node: Node[N, E]) =
+  ## Adds a `node` to the `graph`.  Has no effect if the `node` is already
+  ## in the `graph`.
+  runnableExamples:
+    var g = newGraph[int, string]()
+    g.add 3
+    let n = g[3]
+    g.add n
+    g.add n
+    assert len(g) == 1
+
   if node.id notin graph.members:
     append(graph.nodes, newDoublyLinkedNode(node))
     incl graph.members, node.id
 
 proc add*[N, E](graph: var Graph[N, E]; value: N) =
-  ## append a new node of the given value to the graph
+  ## Creates a new node of `value` and adds it to the `graph`.
   var
     node = newNode[N, E](value)
   graph.add node
 
 proc append[N, E](nodes: var Nodes[N, E]; value: N) =
-  ## append a new node of the given value to the container
+  ## Append a new node of `value` to the `nodes` container.
   var
     node = newNode[N, E](value)
   nodes.append newDoublyLinkedNode(node)
 
-proc contains*[N, E](nodes: Nodes[N, E]; value: N): bool
-  {.deprecated: "not O(1)".} =
-  ## true if the container holds a node with the given value
+proc contains[N, E](nodes: Nodes[N, E]; value: N): bool
+  {.deprecated: "not O(1) yet".} =
+  ## Returns `true` if `nodes` contains a node with the given `value`.
+  # XXX: reimpl using find()
   for node in nodes.items:
     if value == node.value:
       result = true
       break
 
 proc contains*[N, E](edge: Edge[N, E]; value: N): bool =
-  ## true if the edge links a node with the given value
+  ## Returns `true` if `edge` links to a node with the given `value`;
+  ## else `false`.
   result = edge.source.value == value or edge.target.value == value
 
-proc contains*[N, E](edge: Edge[N, E]; target: Node[N, E]): bool =
-  ## true if the edge links the given node
-  result = target.id in {edge.source.id, edge.target.id}
+proc contains*[N, E](edge: Edge[N, E]; node: Node[N, E]): bool =
+  ## Returns `true` if the `edge` links to `node`;
+  ## else `false`.
+  result = node.id in {edge.source.id, edge.target.id}
 
 proc remove[N, E](edges: var Edges[N, E]; edge: Edge[N, E]) =
-  ## not O(1) yet
+  ## Remove an edge from container.
   for item in nodes(edges):
     if item.id == edge.id:
       remove(edges, item)
 
 proc del*[N, E](node: var Node[N, E]; edge: Edge[N, E]) =
-  ## not O(1) yet
+  ## Remove `edge` from `node`.  Of course, this also removes
+  ## `edge` from the `target` node on the opposite side.
+  ## Not O(1) yet; indeed, it is relatively slow!
+  # leave this in a single proc so it's harder to screw up
   if edge.id in node.edges:
-    remove(node.incoming, edge)
-    remove(node.outgoing, edge)
-    excl node.edges, edge.id
+    # remove the source side
+    remove(edge.source.incoming, edge)
+    remove(edge.source.outgoing, edge)
+    excl(edge.source.edges, edge.id)
+    # we can skip removing the target side if this is a "loop"
+    if edge.target.id != edge.source.id:
+      remove(edge.target.incoming, edge)
+      remove(edge.target.outgoing, edge)
+      excl(edge.target.edges, edge.id)
 
 proc add*[N, E](node: var Node[N, E]; edge: Edge[N, E]; target: Node[N, E]) =
-  ## O(1)
+  ## Link `node` to `target` via `edge`; O(1).
   if edge.id notin node.edges:
     append(node.outgoing, newDoublyLinkedNode(edge))
     incl node.peers, target.id
@@ -245,12 +301,18 @@ proc `[]`*[N, E](graph: Graph[N, E]; key: N): Node[N, E] =
         break found
     raise newException(KeyError, "node not found: " & $key)
 
-proc hasKey*[N, E](graph: Graph[N, E]; key: N): bool =
+proc hasKey*[N, E](graph: Graph[N, E]; key: N): bool
+  {.deprecated: "not O(1) yet".} =
   ## true if a node of the given value exists in the container
   for node in graph.nodes.items:
     if node.value == key:
       result = true
       break
+
+iterator items*[N, E](graph: Graph[N, E]): Node[N, E] =
+  ## yield all nodes in the graph
+  for node in lists.items(graph.nodes):
+    yield node
 
 iterator edges*[N, E](graph: Graph[N, E]):
   tuple[source: Node[N, E], edge: Edge[N, E], target: Node[N, E]] =
@@ -258,7 +320,7 @@ iterator edges*[N, E](graph: Graph[N, E]):
   var
     seen = initIntSet()
 
-  for node in graph.nodes.items:
+  for node in graph.items:
     for edge in node.outgoing.items:
       if edge.id notin seen:
         incl seen, edge.id
@@ -269,34 +331,9 @@ iterator edges*[N, E](graph: Graph[N, E]):
         yield (source: edge.source, edge: edge, target: edge.target)
 
 iterator neighbors*[N, E](node: Node[N, E]):
-  tuple[edge: Edge[N, E], node: Node[N, E]] =
+  tuple[edge: Edge[N, E], node: Node[N, E]] {.deprecated: "needs rethink".} =
   ## yield edge and target nodes from a node
   for edge in node.incoming.items:
     yield (edge: edge, node: edge.source)
   for edge in node.outgoing.items:
     yield (edge: edge, node: edge.target)
-
-when false:
-  proc neighbors*[N, E](node: Node[N, E]): Graph[N, E] =
-    result = newGraph()
-    for node in node.edges.items:
-      result.append node
-
-when false:
-  #[
-
-  too hard to reason about
-
-  ]#
-  proc `==`*[N, E](edge: Edge[N, E]; value: E): bool =
-    ## convenience equality for edges and their values
-    result = edge.value == value
-
-  proc `==`*[N, E](node: Node[N, E]; value: N): bool =
-    ## convenience equality for nodes and their values
-    result = node.value == value
-
-iterator items*[N, E](graph: Graph[N, E]): Node[N, E] =
-  ## yield all nodes in the graph
-  for node in lists.items(graph.nodes):
-    yield node
