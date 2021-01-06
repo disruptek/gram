9f1df417c852b2f4c55f477e72fd89480f540236
Andy Davidoff <github@andy.disruptek.com>
2021-01-06 00:29:13 -0500

    rm deprecation

--- gram.nim
+++ gram.nim
@@ -805,12 +805,11 @@ proc nodesAreUnique*[N, E, F](graph: Graph[N, E, F]): bool {.ex.} =
   when ValueIndex in graph.flags:
     result = len(graph.members) == len(graph.hashes)
   else:
-    when (NimMajor, NimMinor) >= (1, 3):
-      var
-        seen = initHashSet[N](initialSize = len(graph))
-    else:
-      var
-        seen = initHashSet[N](initialSize = len(graph).rightSize)
+    var seen =
+      when (NimMajor, NimMinor) >= (1, 4):
+        initHashSet[N](initialSize = len(graph))
+      else:
+        initHashSet[N](initialSize = len(graph).rightSize)
     block found:
       for value in items(graph):
         if value in seen:
